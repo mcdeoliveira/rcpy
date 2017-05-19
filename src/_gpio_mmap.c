3c26e1ef8afd9d75a7d72af0fabb95e3624a55a8
Mauricio de Oliveira <mauricio@ucsd.edu>
2017-05-19 11:38:54 -0700

    gpio

--- src/_gpio_mmap.c
+++ src/_gpio_mmap.c
@@ -3,6 +3,8 @@
 #include <rc_usefulincludes.h>
 #include <roboticscape.h>
 
+#include <stdio.h>
+
 // THIS IS DEFINED IN rc_buttons.h AND rc_defs.h WHICH ARE NOT ACCESSIBLE
 #define POLL_TIMEOUT 100 /* 0.1 seconds */
 #define POLL_BUF_LEN 1024
