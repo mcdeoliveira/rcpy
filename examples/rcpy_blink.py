547117a3b1522dc9d359b899b0e8d0245914269f
Mauricio de Oliveira <mauricio@ucsd.edu>
2017-05-19 15:44:50 -0700

    gpio

--- examples/rcpy_blink.py
+++ examples/rcpy_blink.py
@@ -1,9 +1,3 @@
-if __name__ == "__main__":
-
-    # This is only necessary if package has not been installed
-    import sys
-    sys.path.append('..')
-
 # import Python libraries
 import threading
     
@@ -16,28 +10,22 @@ import rcpy.led as led
 rates = (1, 5, 10)
 index = 0
 
-# start leds
-red = led.blink(led.RED, rates[index % len(rates)], led.ON)
-green = led.blink(led.RED, rates[index % len(rates)], led.OFF)
-blinking = True
-
 # set state to rcpy.RUNNING
 rcpy.set_state(rcpy.RUNNING)
 
-# function to step rate
-def step():
-    while rcpy.get_state() != rcpy.EXITING:
-        if button.pressed(button.MODE):
-            print("<MODE> pressed, stepping blinking rate")
-            # increment rate
-            index += 1
-            red.set_rate(rates[index % len(rates)])
-            green.set_rate(rates[index % len(rates)])
-
-# run step function on a thread
-step_thread = threading.Thread(target=step)
-step_thread.start()
-
+# mode pressed?
+class ModeButtonEvent(button.ButtonEvent):
+    
+    def action(self, event):
+        print("<MODE> pressed, stepping blinking rate")
+        # increment rate
+        index += 1
+        #red.set_rate(rates[index % len(rates)])
+        #green.set_rate(rates[index % len(rates)])
+        
+mode_event = ModeButtonEvent(button.mode, button.Button.PRESSED)
+mode_event.start()
+    
 # welcome message
 print("Green and red LEDs should be flashing")
 print("Press button <MODE> to change the blink rate")
@@ -50,39 +38,39 @@ try:
 
         print("Waiting for <PAUSE> button...")
         # this is a blocking call!
-        if button.pressed(button.PAUSE, 2):
+        if button.pause.pressed():
 
             # pause pressed
             print("<PAUSE> pressed")
 
-            # this is a blocking call with a timeout!
-            if button.released(button.PAUSE, 1.5):
-                # released too soon!
+            # # this is a blocking call with a timeout!
+            # if button.released(button.PAUSE, 1.5):
+            #     # released too soon!
 
-                # toggle start
-                if blinking:
-                    print("Stopped blinking")
-                    # stop leds
-                    red.stop()
-                    green.stop()
-                    blinking = False
+            #     # toggle start
+            #     if blinking:
+            #         print("Stopped blinking")
+            #         # stop leds
+            #         red.stop()
+            #         green.stop()
+            #         blinking = False
 
-                else:
-                    print("Started blinking")
-                    # start leds
-                    red = led.blink(led.RED, rates[index % len(rates)], led.ON)
-                    green = led.blink(led.RED, rates[index % len(rates)], led.OFF)
-                    blinking = True
+            #     else:
+            #         print("Started blinking")
+            #         # start leds
+            #         red = led.blink(led.RED, rates[index % len(rates)], led.ON)
+            #         green = led.blink(led.RED, rates[index % len(rates)], led.OFF)
+            #         blinking = True
 
-            else:
-                # timeout or did not release
-                print("<PAUSE> held, exiting...")
-                # exit
-                break
+            # else:
+            #     # timeout or did not release
+            #     print("<PAUSE> held, exiting...")
+            #     # exit
+            #     break
 
 except KeyboardInterrupt:
     # Catch Ctrl-C
-    rcpy.set_state(rcpy.EXITING)
+    pass
         
 finally:
 
