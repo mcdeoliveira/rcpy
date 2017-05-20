16b370dfb5ac290cc8d98243e130d3a4ecfa88b7
Mauricio de Oliveira <mauricio@ucsd.edu>
2017-05-20 08:30:54 -0700

    tests

--- test/test_led.py
+++ test/test_led.py
@@ -8,20 +8,20 @@ def test1():
 
     led.green.on()
     assert led.green.is_on()
-    assert led.green.pin.is_low()
+    assert gpio.Input(led.green.pin).is_low()
     
     led.green.off()
     assert led.green.is_off()
-    assert led.green.pin.is_high()
+    assert gpio.Input(led.green.pin).is_high()
     
     led.red.on()
     assert led.red.get_state() == led.ON
     assert led.red.is_on()
-    assert led.red.pin.is_low()
+    assert gpio.Input(led.red.pin).is_low()
     
     led.red.off()
     assert led.red.is_off()
-    assert led.red.pin.is_high()
+    assert gpio.Input(led.red.pin).is_high()
 
 if __name__ == '__main__':
 
