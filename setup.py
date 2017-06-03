1044c620739637e854b6d648ed9fdcee1e0d8ff7
Mauricio de Oliveira <mauricio@ucsd.edu>
2017-06-03 10:22:49 -0700

    Preparing to submit to PyPI

--- setup.py
+++ setup.py
@@ -58,9 +58,6 @@ setup(
         'Intended Audience :: Other Audience',
         'Topic :: Scientific/Engineering',
         'License :: OSI Approved :: MIT License',
-        
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3.4',
     ],
     
