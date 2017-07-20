b6824899ef7fbfc2d4b7c013315288855ae1d0b0
Mauricio de Oliveira <mauricio@ucsd.edu>
2017-07-20 11:12:28 -0700

    Custom exit handlers

--- test/test_encoder.py
+++ test/test_encoder.py
@@ -10,39 +10,39 @@ def test1():
     # set to 10
     
     encoder.set(1, 10)
-    assert encoder.read(1) == 10
+    assert encoder.get(1) == 10
 
     encoder.set(2, 10)
-    assert encoder.read(2) == 10
+    assert encoder.get(2) == 10
     
     encoder.set(3, 10)
-    assert encoder.read(3) == 10
+    assert encoder.get(3) == 10
     
     encoder.set(4, 10)
-    assert encoder.read(4) == 10
+    assert encoder.get(4) == 10
 
     # default to zero
     
     encoder.set(1)
-    assert encoder.read(1) == 0
+    assert encoder.get(1) == 0
 
     encoder.set(2)
-    assert encoder.read(2) == 0
+    assert encoder.get(2) == 0
     
     encoder.set(3)
-    assert encoder.read(3) == 0
+    assert encoder.get(3) == 0
     
     encoder.set(4)
-    assert encoder.read(4) == 0
+    assert encoder.get(4) == 0
     
     print('\n ENC #1 |  ENC #2 |  ENC #3 |  ENC #4')
 
     for i in range(N):
 
-        e1 = encoder.read(1)
-        e2 = encoder.read(2)
-        e3 = encoder.read(3)
-        e4 = encoder.read(4)
+        e1 = encoder.get(1)
+        e2 = encoder.get(2)
+        e3 = encoder.get(3)
+        e4 = encoder.get(4)
         
         print('\r{:7d} | {:7d} | {:7d} | {:7d}'.format(e1,e2,e3,e4),
               end='')
