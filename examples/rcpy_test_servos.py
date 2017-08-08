32ba95db540935efc4fc51238b9f71f2ccc2fca7
Mauricio de Oliveira <mauricio@ucsd.edu>
2017-08-07 23:45:15 -0700

    rcpy_test_servos

--- examples/rcpy_test_servos.py
+++ examples/rcpy_test_servos.py
@@ -105,7 +105,7 @@ def main():
                 srvo.set(d)
 
                 # sleep some
-                time.sleep(.1)
+                time.sleep(.02)
 
         # or do nothing
         else:
